5097d509185872712f8f67251820b7ea15d80d11
Harshit Kumar Singh <harshit.ksingh@BLRETV-C02F2RU.local>
2021-03-31 11:40:50 +0530

    changes in bean Package

---
+++ CRS-JAVA-PROJECT-IMP/src/main/java/com/flipkart/bean/AuthDB.java
@@ -0,0 +1,21 @@
+package com.flipkart.bean;
+
+public class AuthDB {
+}
+    Student
+            Professor
+    Admin
+            User
+Role enum.
+        CourseCatalog
+        Course
+        Payment
+        NotificationSystem
+        GradeCard
+        AuthDB
+        Online
+        Offline
+        CourseRegistration
+        Debit
+        Credit
+        PaymentMode
---
+++ CRS-JAVA-PROJECT-IMP/src/main/java/com/flipkart/bean/Online.java
@@ -0,0 +1,21 @@
+package com.flipkart.bean;
+
+public class Online {
+}
+    Student
+            Professor
+    Admin
+            User
+Role enum.
+        CourseCatalog
+        Course
+        Payment
+        NotificationSystem
+        GradeCard
+        AuthDB
+        Online
+        Offline
+        CourseRegistration
+        Debit
+        Credit
+        PaymentMode
