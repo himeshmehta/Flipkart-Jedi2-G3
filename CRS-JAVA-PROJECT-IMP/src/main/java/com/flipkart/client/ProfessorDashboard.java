9a55314825cd23bd578f73beb7181356b6d4d759
Harshit Kumar Singh <harshit.ksingh@BLRETV-C02F2RU.local>
2021-04-05 14:59:33 +0530

    refactor

--- CRS-JAVA-PROJECT-IMP/src/main/java/com/flipkart/client/ProfessorDashboard.java
+++ CRS-JAVA-PROJECT-IMP/src/main/java/com/flipkart/client/ProfessorDashboard.java
@@ -4,12 +4,17 @@ import com.flipkart.bean.Course;
 import com.flipkart.bean.Professor;
 import com.flipkart.bean.Student;
 import com.flipkart.bean.User;
+import com.flipkart.constants.Role;
 import com.flipkart.services.ProfessorServices;
 
+import java.util.Scanner;
+import java.util.logging.Logger;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProfessorDashboard {
+
+    private static final Logger logger = Logger.getLogger(String.valueOf(ProfessorDashboard.class));
 
     private User professor;
     private ProfessorServices professorServices;
@@ -41,4 +46,55 @@ public class ProfessorDashboard {
     {
        professorServices.addGrades(courseId,marks,studentId);
     }
-}
+
+    public void helper()
+    {
+
+          Scanner inputReader = new Scanner(System.in);
+          Boolean result;
+            while (true) {
+                logger.info("Select operation to perform");
+                int operation = inputReader.nextInt();
+                if (operation == -1) break;
+
+                switch (operation) {
+                    case 1:
+
+                        String courseId = inputReader.next();
+
+
+                        Course course=selectCourseToTeach(courseId);
+                        result=course!=null?true:false;
+                        String message = result ? "Course fetched successfully" : "No Course found";
+                        logger.info(message);
+                        break;
+
+                    case 2:
+                         courseId = inputReader.next();
+
+                        List<Student> studentList=getEnrolledStudents(courseId);
+                        result=studentList.isEmpty()?false:true;
+                        message = result ? "List of Students fetched successfully" : "Something Wrong";
+                        logger.info(message);
+                        break;
+
+                    case 3:
+
+                        long marks=Long.parseLong(inputReader.next());
+                        String studentId=inputReader.next();
+                        courseId = inputReader.next();
+
+
+                        addGrades(courseId,marks,studentId);
+
+                        break;
+
+                    default:
+                        logger.info("No operations");
+                        break;
+
+                }
+            }
+        }
+    }
+
