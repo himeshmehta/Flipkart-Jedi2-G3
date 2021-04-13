e0c05a77df27188aea00485c3d21668cf5ad458a
Himesh Mehta <46448677+himeshmehta@users.noreply.github.com>
2021-04-13 13:16:43 +0530

    Delete com directory

--- com/flipkart/Exception/InvalidCourseSelectionException.java
+++
@@ -1,7 +0,0 @@
-package com.flipkart.Exception;
-
-public class InvalidCourseSelectionException extends Exception{
-    public InvalidCourseSelectionException(String message) {
-        super(message);
-    }
-}
