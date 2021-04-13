312da75a0d03db68a0b914d08f61d61bf57fb5d4
Himesh Mehta <himeshmehta319@gmail.com>
2021-04-14 00:53:44 +0530

    Completed the unit test

--- CRS-DROPWIZARD/src/test/java/com/flipkart/LoginTest.java
+++ CRS-DROPWIZARD/src/test/java/com/flipkart/LoginTest.java
@@ -1,2 +1,33 @@
-package com.flipkart;public class LoginTest {
+package com.flipkart;
+
+import com.flipkart.Exception.AuthorizationException;
+import com.flipkart.bean.User;
+import com.flipkart.services.AuthDBServices;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class LoginTest {
+
+    private AuthDBServices authDBServices = new AuthDBServices();
+    @Test
+    public void loginTest1(){
+        // Should throw AuthorizationException  when user id does not exists in database
+        try {
+            authDBServices.authenticateUser(1000,"12345");
+        } catch (AuthorizationException ex) {
+            String errorMessage = ex.getMessage();
+            Assert.assertEquals("user id does not exists.",errorMessage);
+        }
+    }
+
+    @Test
+    public void loginTest2(){
+        // Should throw AuthorizationException when password is incorrect
+        try {
+            authDBServices.authenticateUser(1,"12346");
+        } catch (AuthorizationException ex) {
+            String errorMessage = ex.getMessage();
+            Assert.assertEquals("Incorrect password",errorMessage);
+        }
+    }
 }
--- CRS-DROPWIZARD/src/test/java/com/flipkart/SingUpTest.java
+++ CRS-DROPWIZARD/src/test/java/com/flipkart/SingUpTest.java
@@ -1,2 +1,40 @@
-package com.flipkart;public class SingUpTest {
+package com.flipkart;
+
+import com.flipkart.Exception.CRSException;
+import com.flipkart.services.AuthDBServices;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SingUpTest {
+
+    private AuthDBServices authDBServices = new AuthDBServices();
+    @Test
+    public void signUpTest1(){
+        // should throw CRSException when email is invalid
+        try {
+                authDBServices.selfRegisterStudent("randomEmail","random","12345","12345");
+        } catch (CRSException e) {
+            Assert.assertEquals("Please provide a valid email",e.getMessage());
+        }
+    }
+
+    @Test
+    public void signUpTest2(){
+        // should throw CRSException when password is weak
+        try {
+            authDBServices.selfRegisterStudent("random@gmail.com","random","1234","1234");
+        } catch (CRSException e) {
+            Assert.assertEquals("Weak password. Password should contain minimum 5 digits.",e.getMessage());
+        }
+    }
+
+    @Test
+    public void signUpTest3(){
+        // should throw CRSException when password and confirm password do not matches
+        try {
+            authDBServices.selfRegisterStudent("random@gmail.com","random","123456","12345");
+        } catch (CRSException e) {
+            Assert.assertEquals("Your confirm password do not match with actual password",e.getMessage());
+        }
+    }
 }
